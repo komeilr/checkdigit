56ee7faba1d325b0d100bb382aa314eab34da310
komeilr <komeilr@Gmail.com>
2025-04-03 15:59:33 -0400

    Update checkdigit.py

--- checkdigit.py
+++ checkdigit.py
@@ -1,33 +1,28 @@
-def start():
-  trans_no = input("Please enter a transaction number: ")
-  first_alt = list(str(trans_no[-1::-2]))
-  second_alt = list(str(trans_no[-2::-2]))
-  
-  total = sum([int(x) for x in first_alt]) * 3 + sum([int(x) for x in second_alt])
-  
-  check_digit = (total//10 + 1) * 10 - total
-  if check_digit == 10:
-    check_digit = 0
-  
-  #print(first_alt)
-  #print(second_alt)
-  
-  #print(total)
-  #print(check_digit)
-  
-  print("The check digit is " + str(check_digit))
-  print("Your full transaction number is " + str(trans_no) + '-' + str(check_digit))
+def check_digit(trans:int) -> int:
+    """
+    CBSA 14-digit transaction number checkdigit calculation
+    
+    (a) link together the account security and sequential number into one 13-digit number;   
+    (b) starting with the high-order digit (the most left hand), multiply each digit in an odd-numbered position by 1, and
+    multiply each digit in an even-numbered position by 2;
+    (c) for each result of the multiplication above, if the result is greater than or equal to 10, add the two digits together to
+    give a single digit result (e.g., a digit of 9 multiplied by 2 gives a result of 18. 18 is greater than 10, so add the digits 1 and
+    8 together to give a result of 9);
+    (d) sum all of the results calculated above;
+    (e) divide the sum by 10, giving a quotient and a remainder;
+    (f) the remainder becomes the check digit and the 14th digit of the transaction number.
+    """
+    
+    assert len(str(trans))==13, "trans argument must be of length 13"
 
-def check_digit_oneline():
-  trans_no = input("Please enter a transaction number: ")
-  
-  # rewrite this line, asshole it's not readable!
-  check_digit = ((sum([int(x) for x in list(str(trans_no[-1::-2]))]) * 3 + sum([int(x) for x in list(str(trans_no[-2::-2]))]))//10 + 1) * 10 - (sum([int(x) for x in list(str(trans_no[-1::-2]))]) * 3 + sum([int(x) for x in list(str(trans_no[-2::-2]))]))
-  
-  if check_digit == 10:
-    check_digit = 0
-  
-  print("Your checkdigit is " + str(check_digit))
+    odds = [int(n) for idx, n in enumerate(str(trans)) if idx%2 == 0]
+    evens = [int(n) for idx, n in enumerate(str(trans)) if idx%2 == 1]
+    odd_sum = sum(odds)
+    even_sum = 0
+    for n in evens:
+        result = n * 2
+        if result > 10:
+            result = sum([int(i) for i in str(result)])
+        even_sum += result
 
-#start()
-check_digit_oneline()
+    return f"{trans}{(odd_sum + even_sum) % 10}"
